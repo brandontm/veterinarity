39bd8e4dc2fa697762012eaa3f2674d4cb92de0b
brandontm <brandon_btm@outlook.com>
2020-10-18 23:30:14 -0600

    Initial Commit

---
+++ src/test/java/com/github/brandontm/veterinarity/veterinarity/VeterinarityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.brandontm.veterinarity.veterinarity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeterinarityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
