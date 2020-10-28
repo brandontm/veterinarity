bf0cc21f099f776ec513a3e5c4b215f98999a2f3
brandont <brandon_btm@outlook.com>
2020-10-28 00:26:40 -0700

    Se crean entidades

---
+++ src/main/java/com/github/brandontm/veterinarity/veterinarity/entity/Cliente.java
@@ -0,0 +1,72 @@
+package com.github.brandontm.veterinarity.veterinarity.entity;
+
+public class Cliente {
+    private Integer id;
+    private String nombre;
+    private String apellidoPaterno;
+    private String apellidoMaterno;
+    private String telefono;
+    private String celular;
+    private Integer idDomicilio;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Integer getIdDomicilio() {
+        return idDomicilio;
+    }
+
+    public void setIdDomicilio(Integer idDomicilio) {
+        this.idDomicilio = idDomicilio;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    
+
+
+    // other fields, getters and setters
+}
