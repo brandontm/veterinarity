57759c2d7c0a0f178775b430083a65915eb5ca27
brandontm <brandon_btm@outlook.com>
2020-10-30 02:47:11 -0700

    Permitir ligar dueño a mascota Mostrar información de mascota

--- src/main/java/com/github/brandontm/veterinarity/veterinarity/clientes/ClientesController.java
+++ src/main/java/com/github/brandontm/veterinarity/veterinarity/clientes/ClientesController.java
@@ -2,6 +2,8 @@ package com.github.brandontm.veterinarity.veterinarity.clientes;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,36 +15,36 @@ import javax.servlet.ServletContext;
 
 @Controller
 public class ClientesController {
-	@Autowired
+    @Autowired
     ServletContext servletContext;
 
-	@GetMapping("/cliente")
-	public String clientes() {
+    @GetMapping("/cliente")
+    public String clientes() {
         List<Cliente> clientes = (List<Cliente>) servletContext.getAttribute("clientes");
         if (clientes == null)
             clientes = new ArrayList<Cliente>();
-        
 
         servletContext.setAttribute("clientes", clientes);
 
-		return "clientes";
-	}
+        return "clientes";
+    }
 
-	@GetMapping("/cliente/agregar")
+    @GetMapping("/cliente/agregar")
     public String agregarClienteForm(Model model) {
         model.addAttribute("cliente", new Cliente());
         return "agregar_cliente";
-	}
-	
-	@PostMapping("/cliente/agregar")
+    }
+
+    @PostMapping("/cliente/agregar")
     public String agregarCliente(@ModelAttribute(value = "cliente") Cliente cliente) {
         List<Cliente> clientes = (List<Cliente>) servletContext.getAttribute("clientes");
         if (clientes == null)
             clientes = new ArrayList<Cliente>();
-        
-        
 
-		clientes.add(cliente);
+        int id = clientes.stream().flatMapToInt(c -> IntStream.of(c.getId())).max().orElse(0) + 1; // get next id
+        cliente.setId(id);
+
+        clientes.add(cliente);
         servletContext.setAttribute("clientes", clientes);
         return "agregar_cliente";
     }
--- src/main/java/com/github/brandontm/veterinarity/veterinarity/pacientes/PacientesController.java
+++ src/main/java/com/github/brandontm/veterinarity/veterinarity/pacientes/PacientesController.java
@@ -2,9 +2,11 @@ package com.github.brandontm.veterinarity.veterinarity.pacientes;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 import javax.servlet.ServletContext;
 
+import com.github.brandontm.veterinarity.veterinarity.entity.Cliente;
 import com.github.brandontm.veterinarity.veterinarity.entity.Pet;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +15,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PacientesController {
@@ -21,26 +24,48 @@ public class PacientesController {
 
     @GetMapping("/pacientes")
     public String pacientes() {
+        if (servletContext.getAttribute("pets") == null)
+            servletContext.setAttribute("pets", new ArrayList<Pet>());
+
         return "pacientes";
     }
 
     @GetMapping("/paciente/agregar")
     public String agregarPacienteForm(Model model) {
         Pet pet = new Pet();
-        pet.setSex("male");
+        pet.setSex("macho");
         model.addAttribute("pet", pet);
-        
+
         return "agregar_paciente";
     }
 
     @PostMapping("/paciente/agregar")
-    public String agregarPaciente(@ModelAttribute(value = "pet") Pet pet) {
+    public String agregarPaciente(@ModelAttribute(value = "pet") Pet pet, @RequestParam("idCliente") int idCliente) {
         List<Pet> pets = (List<Pet>) servletContext.getAttribute("pets");
         if (pets == null)
             pets = new ArrayList<Pet>();
 
+        int id = pets.stream().flatMapToInt(p -> IntStream.of(p.getId())).max().orElse(0) + 1; // get next id
+        pet.setId(id);
         pets.add(pet);
         servletContext.setAttribute("pets", pets);
+
+        List<Cliente> clientes = (List<Cliente>) servletContext.getAttribute("clientes");
+        if (clientes == null)
+            clientes = new ArrayList<Cliente>();
+
+        for (int i = 0; i < clientes.size(); i++) {
+            final Cliente c = clientes.get(i);
+            if (c.getId() != idCliente)
+                continue;
+
+            List<Pet> petList = c.getPets();
+            petList.add(pet);
+            c.setPets(petList);
+
+            clientes.set(i, c);
+            servletContext.setAttribute("clientes", clientes);
+        }
         return "agregar_paciente";
     }
 }
