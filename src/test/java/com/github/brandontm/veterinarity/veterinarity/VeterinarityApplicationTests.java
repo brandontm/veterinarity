c13cb573696088d65bf6b9afab94a28231b707ab
brandontm <brandon_btm@outlook.com>
2020-11-01 18:55:47 -0700

    Se agrega prueba de humo

--- src/test/java/com/github/brandontm/veterinarity/veterinarity/VeterinarityApplicationTests.java
+++ src/test/java/com/github/brandontm/veterinarity/veterinarity/VeterinarityApplicationTests.java
@@ -1,13 +1,20 @@
 package com.github.brandontm.veterinarity.veterinarity;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class VeterinarityApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private VeterinarityApplication application;
+
+    @Test
+    public void contextLoads() throws Exception {
+        assertThat(application).isNotNull();
+    }
 
 }
