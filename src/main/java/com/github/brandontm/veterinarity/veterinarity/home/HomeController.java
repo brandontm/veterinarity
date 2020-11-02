000b8d8f8c0f3f26e8b6d5a15680338d5f3dfe3a
brandontm <brandon_btm@outlook.com>
2020-11-01 18:49:58 -0700

    Se arreglan errores de navegacion

--- src/main/java/com/github/brandontm/veterinarity/veterinarity/home/HomeController.java
+++ src/main/java/com/github/brandontm/veterinarity/veterinarity/home/HomeController.java
@@ -1,16 +1,12 @@
-package com.github.brandontm.veterinarity.veterinarity.clientes;
+package com.github.brandontm.veterinarity.veterinarity.home;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.IntStream;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.github.brandontm.veterinarity.veterinarity.entity.Cliente;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import javax.servlet.ServletContext;
 
 @Controller
